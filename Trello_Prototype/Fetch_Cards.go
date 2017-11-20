cda1a4180a4fe9fd994051df62a74a95c89c0475
Steven Mackey <stevester94@gmail.com>
2017-11-19 18:16:20 -0800

    More comments, make data structures actually mirror how the request looks

--- Trello_Prototype/Fetch_Cards.go
+++ Trello_Prototype/Fetch_Cards.go
@@ -5,6 +5,7 @@ import "net/http"
 import "io/ioutil"
 import "os"
 import "encoding/json"
+import "bytes"
 
 const API_Key string = "f44bfd10a8cfed257d7dc6cb1ba18038"
 const Token string = "90f6221553f46fb8bcedd905e3f74cd7e13327336c7f18ec6754620bf520627f"
@@ -12,23 +13,33 @@ const Board_ID string = "db6BDqSJ"
 const Endpoint_format_string string = "https://api.trello.com/1/boards/%s/cards?key=%s&token=%s" // Board_ID, key, token
 
 
+// We expect to get a slice of maps from the input JSON
+// Each element is a card
+// We are getting cards because that's which endpoint we requested
 func deJsonifyCardArray(s []byte) {
-	var dat = make([]interface{}, 50)
+	var dat = make([]map[string]interface{}, 50)
 
 	if err := json.Unmarshal(s, &dat); err != nil {
         panic(err)
     }
 
+    // Strictly for Debugging what we got
+    var prettyJSON bytes.Buffer
+    json.Indent(&prettyJSON, s, "", "    ")
+
+    fmt.Println("Payload:")
+    fmt.Println(prettyJSON.String())
+
+
     fmt.Println("All fields:")
-    for  key, _ := range dat[0].(map[string]interface{}) {
+    for  key, _ := range dat[0] {
         fmt.Printf("%s ", key)
     }
 
     fmt.Println()
 
     fmt.Println("Index, name, dateLastActivity")
-    for index, element := range dat {
-        card_map := element.(map[string]interface{})
+    for index, card_map := range dat {
         fmt.Printf("%d: %s, %s\n", index, card_map["name"], card_map["dateLastActivity"])
     }
 }
